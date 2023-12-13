d166a001bc202a147b85012de538fed0e445cc66
Klaudija <buci_klau@yahoo.com>
2023-12-13 10:11:09 +0100

    test

--- my-app/src/components/Users/MyProfil.tsx
+++ my-app/src/components/Users/MyProfil.tsx
@@ -37,7 +37,7 @@ const numberOfDonation=filterUsersLS.length;
               />
             </div>
             <div className="col-12 col-lg-6 d-flex flex-column align-items-center align-items-lg-start">
-              <h1 className="mt-5 custom-color-01">{name}</h1>{" "}
+              <h1 className="mt-5 custom-color-01">{name}</h1>{"test "}
               <div className="my-3">
                 <Link to={"https://www.facebook.com"}>  <FacebookIcon
                   className="custom-color-01"
